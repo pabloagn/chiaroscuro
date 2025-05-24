c473a459cf9229b42c10586c1d799a228c2f309b
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-05-24 14:48:32 +0100

    Added nix token generator

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
-# flake.nix
-
+# phantom-theme/flake.nix
 {
   description = "Flake for Chiaroscuro";
 
@@ -17,24 +16,44 @@
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.sass
+            pkgs.gawk
+            pkgs.gnugrep
+            pkgs.gnused
           ];
         };
 
-        packages.chiaroscuro = pkgs.stdenv.mkDerivation {
-          pname = "chiaroscuro";
+        packages.phantom-provider = pkgs.stdenv.mkDerivation {
+          pname = "phantom-provider";
           version = "0.1.0";
           src = ./.;
-          nativeBuildInputs = [ pkgs.sass ];
+
+          nativeBuildInputs = [
+            pkgs.sass
+            pkgs.gawk
+            pkgs.gnugrep
+            pkgs.coreutils
+          ];
 
           buildPhase = ''
             runHook preBuild
-            mkdir -p $out/dist
-            sass src/main.scss $out/dist/chiaroscuro.css --style=expanded
+
+            mkdir -p dist
+
+            echo "Compiling SCSS to dist/chiaroscuro.css..."
+            sass src/main.scss dist/chiaroscuro.css --style=expanded
+
+            echo "Generating Nix tokens to dist/chiaroscuro.nix..."
+            chmod +x scripts/generate-nix-tokens.sh
+            ./scripts/generate-nix-tokens.sh dist/chiaroscuro.css dist/chiaroscuro.nix
+
             runHook postBuild
           '';
 
           installPhase = ''
             runHook preInstall
+            # Copy the entire dist directory to $out
+            mkdir -p $out
+            cp -r dist/* $out/
             runHook postInstall
           '';
         };
