a831ef9f066d03d6a1eb7283d656fbfccfae4e6e
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-05-24 21:14:56 +0100

    Completed Rust util

--- target_dev/debug/deps/chiaroscuro_parser-606e7671efedbeb7.d
+++
@@ -1,5 +0,0 @@
-/home/pabloagn/projects/chiaroscuro/target_dev/debug/deps/libchiaroscuro_parser-606e7671efedbeb7.rmeta: src/main.rs
-
-/home/pabloagn/projects/chiaroscuro/target_dev/debug/deps/chiaroscuro_parser-606e7671efedbeb7.d: src/main.rs
-
-src/main.rs:
