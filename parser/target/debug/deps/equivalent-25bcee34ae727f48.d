c6560b15fc0e8fee5483fd78b58a64af18cc72a2
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-05-24 15:43:53 +0100

    Added Rust parser util

--- parser/target/debug/deps/equivalent-25bcee34ae727f48.d
+++
@@ -1,5 +0,0 @@
-/home/pabloagn/projects/chiaroscuro/parser/target/debug/deps/libequivalent-25bcee34ae727f48.rmeta: /home/pabloagn/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
-
-/home/pabloagn/projects/chiaroscuro/parser/target/debug/deps/equivalent-25bcee34ae727f48.d: /home/pabloagn/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
-
-/home/pabloagn/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs:
