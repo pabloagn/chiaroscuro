443ddc4c31e0c141624452723d05db19c2865c0c
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-10 15:46:26 +0100

    Added ports

--- ports/kitty.nix
+++ ports/kitty.nix
@@ -5,41 +5,40 @@ let
   palette = colors.${variant} or colors.zen;
 in
 {
-  background = palette.sumiInk1;
-  foreground = palette.fujiWhite;
+  background = palette.zen0;
+  foreground = palette.oldWhite;
   
-  selection_background = palette.waveBlue2;
-  selection_foreground = palette.fujiWhite;
+  selection_background = palette.zenBlue2;
+  selection_foreground = palette.oldWhite;
   
-  cursor = palette.fujiWhite;
-  cursor_text_color = palette.sumiInk1;
+  cursor = palette.oldWhite;
+  cursor_text_color = palette.zen0;
   
   url_color = palette.springBlue;
   
-  active_tab_background = palette.sumiInk1;
-  active_tab_foreground = palette.fujiWhite;
-  inactive_tab_background = palette.sumiInk1;
-  inactive_tab_foreground = palette.sumiInk6;
+  active_tab_background = palette.zen0;
+  active_tab_foreground = palette.oldWhite;
+  inactive_tab_background = palette.zen0;
+  inactive_tab_foreground = palette.katanaGray;
   
-  color0 = palette.sumiInk0;
+  color0 = palette.zen0;
   color1 = palette.autumnRed;
   color2 = palette.autumnGreen;
-  color3 = palette.boatYellow2;
-  color4 = palette.crystalBlue;
-  color5 = palette.oniViolet;
-  color6 = palette.waveAqua1;
+  color3 = palette.autumnYellow;
+  color4 = palette.springBlue;
+  color5 = palette.springViolet1;
+  color6 = palette.zenAqua1;
   color7 = palette.oldWhite;
   
-  color8 = palette.fujiGray;
-  color9 = palette.samuraiRed;
+  color8 = palette.katanaGray;
+  color9 = palette.zenRed;
   color10 = palette.springGreen;
   color11 = palette.carpYellow;
   color12 = palette.springBlue;
   color13 = palette.springViolet1;
-  color14 = palette.waveAqua2;
+  color14 = palette.zenAqua2;
   color15 = palette.fujiWhite;
   
-  color16 = palette.surimiOrange;
-  color17 = palette.peachRed;
+  color16 = palette.roninYellow;
+  color17 = palette.samuraiRed;
 }
-
