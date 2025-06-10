e6edc64a0e71507690cd5e31ad979dff21a4fea2
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-10 15:33:22 +0100

    Added ports

--- flake.nix
+++ flake.nix
@@ -28,29 +28,29 @@
       themes = {
         kanso-zen = {
           palette = kansoColors.zen;
-          kitty = ports.generators.kitty { colors = kansoColors; variant = "zen"; };
-          ghostty = ports.generators.ghostty { colors = kansoColors; variant = "zen"; };
-          yazi = ports.generators.yazi { colors = kansoColors; variant = "zen"; };
-          lualine = ports.generators.lualine { colors = kansoColors; variant = "zen"; };
-          nvim = ports.generators.nvim { colors = kansoColors; variant = "zen"; };
+          kitty = ports.kitty { colors = kansoColors; variant = "zen"; };
+          ghostty = ports.ghostty { colors = kansoColors; variant = "zen"; };
+          yazi = ports.yazi { colors = kansoColors; variant = "zen"; };
+          lualine = ports.lualine { colors = kansoColors; variant = "zen"; };
+          nvim = ports.nvim { colors = kansoColors; variant = "zen"; };
         };
         
         kanso-ink = {
           palette = kansoColors.ink;
-          kitty = ports.generators.kitty { colors = kansoColors; variant = "ink"; };
-          ghostty = ports.generators.ghostty { colors = kansoColors; variant = "ink"; };
-          yazi = ports.generators.yazi { colors = kansoColors; variant = "ink"; };
-          lualine = ports.generators.lualine { colors = kansoColors; variant = "ink"; };
-          nvim = ports.generators.nvim { colors = kansoColors; variant = "ink"; };
+          kitty = ports.kitty { colors = kansoColors; variant = "ink"; };
+          ghostty = ports.ghostty { colors = kansoColors; variant = "ink"; };
+          yazi = ports.yazi { colors = kansoColors; variant = "ink"; };
+          lualine = ports.lualine { colors = kansoColors; variant = "ink"; };
+          nvim = ports.nvim { colors = kansoColors; variant = "ink"; };
         };
         
         kanso-pearl = {
           palette = kansoColors.pearl;
-          kitty = ports.generators.kitty { colors = kansoColors; variant = "pearl"; };
-          ghostty = ports.generators.ghostty { colors = kansoColors; variant = "pearl"; };
-          yazi = ports.generators.yazi { colors = kansoColors; variant = "pearl"; };
-          lualine = ports.generators.lualine { colors = kansoColors; variant = "pearl"; };
-          nvim = ports.generators.nvim { colors = kansoColors; variant = "pearl"; };
+          kitty = ports.kitty { colors = kansoColors; variant = "pearl"; };
+          ghostty = ports.ghostty { colors = kansoColors; variant = "pearl"; };
+          yazi = ports.yazi { colors = kansoColors; variant = "pearl"; };
+          lualine = ports.lualine { colors = kansoColors; variant = "pearl"; };
+          nvim = ports.nvim { colors = kansoColors; variant = "pearl"; };
         };
       };
       
@@ -65,3 +65,4 @@
       };
     };
 }
+
