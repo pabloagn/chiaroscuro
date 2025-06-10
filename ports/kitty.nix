445d2903cd2fa6fd2a43d4bf8ef877c9fb279952
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-10 15:30:05 +0100

    Added ports

--- ports/kitty.nix
+++ ports/kitty.nix
@@ -5,56 +5,41 @@ let
   palette = colors.${variant} or colors.zen;
 in
 {
-  colors = {
-    primary = {
-      background = palette.sumiInk1 or "#1F1F28";
-      foreground = palette.fujiWhite or "#DCD7BA";
-    };
-    
-    cursor = {
-      text = palette.sumiInk1 or "#1F1F28";
-      cursor = palette.fujiWhite or "#DCD7BA";
-    };
-    
-    selection = {
-      background = palette.waveBlue2 or "#2D4F67";
-      foreground = palette.fujiWhite or "#DCD7BA";
-    };
-    
-    normal = {
-      black = palette.sumiInk0 or "#16161D";
-      red = palette.autumnRed or "#C34043";
-      green = palette.autumnGreen or "#76946A";
-      yellow = palette.boatYellow2 or "#C0A36E";
-      blue = palette.crystalBlue or "#7E9CD8";
-      magenta = palette.oniViolet or "#957FB8";
-      cyan = palette.waveAqua1 or "#6A9589";
-      white = palette.oldWhite or "#C8C093";
-    };
-    
-    bright = {
-      black = palette.fujiGray or "#727169";
-      red = palette.samuraiRed or "#E82424";
-      green = palette.springGreen or "#98BB6C";
-      yellow = palette.carpYellow or "#E6C384";
-      blue = palette.springBlue or "#7FB4CA";
-      magenta = palette.springViolet1 or "#938AA9";
-      cyan = palette.waveAqua2 or "#7AA89F";
-      white = palette.fujiWhite or "#DCD7BA";
-    };
-    
-    # Kitty-specific
-    url_color = palette.springBlue or "#7FB4CA";
-    
-    tab_bar = {
-      active = {
-        background = palette.sumiInk1 or "#1F1F28";
-        foreground = palette.fujiWhite or "#DCD7BA";
-      };
-      inactive = {
-        background = palette.sumiInk1 or "#1F1F28";
-        foreground = palette.sumiInk6 or "#727169";
-      };
-    };
-  };
+  background = palette.sumiInk1;
+  foreground = palette.fujiWhite;
+  
+  selection_background = palette.waveBlue2;
+  selection_foreground = palette.fujiWhite;
+  
+  cursor = palette.fujiWhite;
+  cursor_text_color = palette.sumiInk1;
+  
+  url_color = palette.springBlue;
+  
+  active_tab_background = palette.sumiInk1;
+  active_tab_foreground = palette.fujiWhite;
+  inactive_tab_background = palette.sumiInk1;
+  inactive_tab_foreground = palette.sumiInk6;
+  
+  color0 = palette.sumiInk0;
+  color1 = palette.autumnRed;
+  color2 = palette.autumnGreen;
+  color3 = palette.boatYellow2;
+  color4 = palette.crystalBlue;
+  color5 = palette.oniViolet;
+  color6 = palette.waveAqua1;
+  color7 = palette.oldWhite;
+  
+  color8 = palette.fujiGray;
+  color9 = palette.samuraiRed;
+  color10 = palette.springGreen;
+  color11 = palette.carpYellow;
+  color12 = palette.springBlue;
+  color13 = palette.springViolet1;
+  color14 = palette.waveAqua2;
+  color15 = palette.fujiWhite;
+  
+  color16 = palette.surimiOrange;
+  color17 = palette.peachRed;
 }
+
